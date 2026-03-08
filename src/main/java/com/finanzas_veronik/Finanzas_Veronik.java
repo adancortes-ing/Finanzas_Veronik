81f334fa3f88deb291c197c9b53e3f3dcc24bb88
Adán Cortés <adan.cortes.ing@gmail.com>
2026-03-08 16:59:48 -0600

    chore: Estructura inicial del proyecto

---
+++ src/main/java/com/finanzas_veronik/Finanzas_Veronik.java
@@ -0,0 +1,13 @@
+package com.finanzas_veronik;
+
+/**
+ *
+ * @author Adán Cortes (CorLogic)
+ * @version 0.1.0
+ */
+public class Finanzas_Veronik {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
